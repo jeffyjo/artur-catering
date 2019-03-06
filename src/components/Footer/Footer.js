d5bae2b94aeb7b66c330b02958a2efeec0c788c0
Jeff Yahin Jørgensen <Jeff@Jeffs-MacBook-Pro.local>
2019-03-06 18:37:00 +0100

    Add ITCSS

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,6 +4,8 @@ import { Link } from 'gatsby'
 import facebook from '../../img/social/facebook.svg'
 import instagram from '../../img/social/instagram.svg'
 
+import './Footer.scss'
+
 const Footer = class extends React.Component {
   render() {
     return (
@@ -16,23 +18,11 @@ const Footer = class extends React.Component {
                     <ul className="menu-list">
                       <li><Link to="/" className="navbar-item">Home</Link></li>
                       <li><Link className="navbar-item" to="/about">About</Link></li>
-                      <li><Link className="navbar-item" to="/products">
-                        Products
-                      </Link>
-                    </li>
-                    <li>
-                    <Link className="navbar-item" to="/contact/examples">
-                      Form Examples
-                    </Link>
-                    </li>
-                    <li><a
-                      className="navbar-item"
-                      href="/admin/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Admin
-                    </a></li>
+                      <li>
+                        <Link className="navbar-item" to="/contact/examples">
+                          Form Examples
+                        </Link>
+                      </li>
                     </ul>
                   </section>
                 </div>
---
+++ src/styles/Readme.md
@@ -0,0 +1,2 @@
+The structure in styles/ is inspired by ITCSS-architecture:
+https://www.xfive.co/blog/itcss-scalable-maintainable-css-architecture/
