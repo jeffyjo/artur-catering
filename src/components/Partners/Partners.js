7056dee148b0d2f89c649b0ca91cbd25bdb6d628
Jeff Yahin Jørgensen <Jeff@Jeffs-MacBook-Pro.local>
2019-03-13 19:08:16 +0100

    Add partners component

---
+++ src/components/Partners/Partners.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import Partner from './Partner/Partner'
+
+import './Partners.scss'
+
+const Partners = ({ partners }) => {
+    return (
+        <div className="o-partners">
+            {/* Should dynamically loop and ouput content */}
+            <Partner className="o-partners__partner" />
+            <Partner className="o-partners__partner" />
+            <Partner className="o-partners__partner" />
+            <Partner className="o-partners__partner" />
+            <Partner className="o-partners__partner" />
+        </div>
+    )
+}
+
+Partners.propTypes = {
+    partners: PropTypes.shape({
+        src: PropTypes.string,
+        imageTitle: PropTypes.string,
+        partnerName: PropTypes.string
+    })
+}
+
+export default Partners
