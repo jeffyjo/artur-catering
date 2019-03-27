858a8e795cc2d815eaf6ec8072377e19addf9c71
Jeff Yahin Jørgensen <Jeff@Jeffs-MacBook-Pro.local>
2019-03-27 17:56:55 +0100

    Update contact page

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -43,7 +43,7 @@ export default class Index extends React.Component {
               <form
                 name="contact"
                 method="post"
-                action="/contact/thanks/"
+                action="/thanks/"
                 data-netlify="true"
                 data-netlify-honeypot="bot-field"
                 onSubmit={this.handleSubmit}
