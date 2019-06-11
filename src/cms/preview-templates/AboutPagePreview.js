21082ca56c788141a411cfb2f93618d2835abef3
Marek Kisiel <mr.kislun@gmail.com>
2019-06-11 15:01:40 +0200

    preview

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { AboutPageTemplate } from '../../templates/about-page'
 
-const AboutPagePreview = ({ entry, getAsset}) => {
+const AboutPagePreview = ({ entry}) => {
   const entryAvatars = entry.getIn(['data','avatars']);
   const avatars = entryAvatars ? entryAvatars.toJS() : []
 
