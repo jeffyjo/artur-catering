1ea7c9278905b4c861af2d2eb696ecd07314757b
Marek Kisiel <mr.kislun@gmail.com>
2019-06-11 15:01:32 +0200

    Preview for about

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -2,14 +2,22 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { AboutPageTemplate } from '../../templates/about-page'
 
-const AboutPagePreview = ({ entry }) => (
-  <AboutPageTemplate
-    title={entry.getIn(['data', 'title'])}
-    avatars={entry.getIn(['data', 'avatars'])}
-    partners={entry.getIn(['data', 'partners'])}
-    content={entry.getIn(['body'])}
-  />
-)
+const AboutPagePreview = ({ entry, getAsset}) => {
+  const entryAvatars = entry.getIn(['data','avatars']);
+  const avatars = entryAvatars ? entryAvatars.toJS() : []
+
+  const entryPartners = entry.getIn(['data','partners']);
+  const partners = entryPartners ? entryPartners.toJS() : []
+
+  return ( 
+    <AboutPageTemplate
+      title={entry.getIn(['data', 'title'])}
+      avatars={avatars}
+      partners={partners}
+      content={entry.getIn(['body'])}
+    />
+  )
+}
 
 AboutPagePreview.propTypes = {
   entry: PropTypes.shape({
