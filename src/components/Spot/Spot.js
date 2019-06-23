ea7c8003a4ad2acf00062dd3aba0788b1a5d4786
jeffyjo <jeff_1992_1992@hotmail.com>
2019-06-23 19:23:11 +0200

    Update spot with html-tag and remove Content

--- src/components/Spot/Spot.js
+++ src/components/Spot/Spot.js
@@ -1,23 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import Content from "../Util/Content";
 import Image from "../Image/Image";
 
 import "./Spot.scss";
 
 const Spot = ({ src, title, contentComponent, className }) => {
-  const PageContent = contentComponent || Content;
+  const PageContent = contentComponent;
 
   return (
-    <div className={`o-spot ${className ? className : ""}`}>
+    <section className={`o-spot ${className ? className : ""}`}>
       {/* <PageContent /> */}
       <div className="o-spot__content">{PageContent}</div>
 
       <div className="o-spot__image-wrapper">
         <Image src={src} title={title} />
       </div>
-    </div>
+    </section>
   );
 };
 
